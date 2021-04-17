36011d0ff4f1b0f3c546cc208f424c677371fa70
James Mackay <31529245+manuforti@users.noreply.github.com>
2021-04-16 18:23:23 -0700

    Generate migrations for schema

---
+++ console/migrations/m210416_023958_create_order_items_table.php
@@ -0,0 +1,91 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%order_items}}`.
+ */
+class m210416_023958_create_order_items_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%order_items}}', [
+            'id' => $this->primaryKey(),
+            'product_name' => $this->string(255)->notNull(),
+            'product_id' => $this->integer(11)->notNull(),
+            'unit_price' => $this->decimal(10, 2)->notNull(),
+            'order_id' => $this->integer(11)->notNull(),
+            'quantity' => $this->integer(2)->notNull(),
+        ]);
+
+        // creates index for column `product_id`
+        $this->createIndex(
+            '{{%idx-order_items-product_id}}',
+            '{{%order_items}}',
+            'product_id'
+        );
+
+        // add foreign key for table `{{%order_items}}`
+        $this->addForeignKey(
+            '{{%fk-order_items-product_id}}',
+            '{{%order_items}}',
+            'product_id',
+            '{{%products}}',
+            'id',
+            'CASCADE'
+        );
+
+        // creates index for column `order_id`
+        $this->createIndex(
+            '{{%idx-order_items-order_id}}',
+            '{{%order_items}}',
+            'order_id'
+        );
+
+        // add foreign key for table `{{%order_items}}`
+        $this->addForeignKey(
+            '{{%fk-order_items-order_id}}',
+            '{{%order_items}}',
+            'order_id',
+            '{{%orders}}',
+            'id',
+            'CASCADE'
+        );
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%order_items}}`
+        $this->dropForeignKey(
+            '{{%fk-order_items-product_id}}',
+            '{{%order_items}}'
+        );
+
+        // drops index for column `product_id`
+        $this->dropIndex(
+            '{{%idx-order_items-product_id}}',
+            '{{%order_items}}'
+        );
+        $this->dropTable('{{%order_items}}');
+
+        // drops foreign key for table `{{%order_items}}`
+        $this->dropForeignKey(
+            '{{%fk-order_items-order_id}}',
+            '{{%order_items}}'
+        );
+
+        // drops index for column `order_id`
+        $this->dropIndex(
+            '{{%idx-order_items-order_id}}',
+            '{{%order_items}}'
+        );
+        $this->dropTable('{{%order_items}}');
+    }
+}
